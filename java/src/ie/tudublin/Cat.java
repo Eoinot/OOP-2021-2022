eb575e5839f85c00769fcf52e005449ef2551056
Eoin <eoinot128@gmail.com>
2022-01-26 15:28:05 +0000

    Killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,30 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives;
+    public int lives(){
+        return numLives;
+    }
+
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
     }
+
+    public void Kill(int newLives)
+    {
+
+        numLives = newLives;
+        while (numLives > 0)
+        {
+            if (numLives > 0)
+            {
+                numLives = numLives - 1;
+                System.out.println("Ouch");
+            } 
+        }  
+        System.out.println("Dead"); 
+
+    }   
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -4,16 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+      
 
-        Animal misty = new Animal("Misty");
-        Animal lucy = new Animal("Lucy");
+        Cat ginger = new Cat("Ginger");
+        ginger.Kill(9);
 
-        lucy = misty;
-        misty.setName("Tara");
+     
 
-        System.out.println(misty);
-        System.out.println(lucy);
 
         // Tara Misty
         // Tara Tara
