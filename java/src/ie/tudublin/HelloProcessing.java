33ae61e06b43b0d8c892039dffc68ea4adf9a964
Eoin <eoinot128@gmail.com>
2022-02-01 12:56:33 +0000

    illuminate symbol

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,7 +11,7 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 
 		
@@ -19,6 +19,36 @@ public class HelloProcessing extends PApplet
 
 	public void draw()
 	{	
+		noStroke();
+		background(255,0,0);
+
+		//line(10, 10, 200, 200);// x1,y1,x2,y2
+		fill(255,255,0);
+		circle(250, 300, 400); //cx,cy,r 
+		noFill();
+		fill(137, 207, 240);
+		triangle(250, 20, 50, 450,450, 450); //x1,y1,x2,y2,x3,y3
+		noFill();
+		fill(244, 194, 194);
+		ellipse(250, 250, 200, 90); // cx,cx,w,h
+		noFill();
+		fill(0);,
+		circle(250, 250, 65); //cx,cy,r 
+		noFill();
+
+		/* stroke(0);
+		fill(0,0,255);
+		nofill();
+		nostroke(); */
+
+		/* rectMode(CENTER);
+		rect(300,20,100,20); //tlx,tly,w,h
+
+		rectMode(CORNER);
+		rect(200,20,100,20); //tlx,tly,w,h
+
+		point(20,300);
+ */
 		
 	}
 }
