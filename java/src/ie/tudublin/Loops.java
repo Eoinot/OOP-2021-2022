d1a5dd73b2179bb5f2abc4b6e7b363937fac6f48
Eoin <eoinot128@gmail.com>
2022-02-14 11:31:42 +0000

    More lab week 3 study

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -4,6 +4,32 @@ import processing.core.PApplet;
 
 public class Loops extends PApplet
 {
+	/* q1 static int a = 0;
+	static int b = 0;
+	static int c = 50;
+	static int d = 500; 
+
+	 Q2 static int a = 0;
+	static int b = 0;
+	static int c = 50;
+	static int d = 50; 
+
+	Q3 static int a = 0;
+	static int b = 0;
+	static int c = 50;
+	static int d = 50; 
+
+	Q4 static int e = 450;
+	static int f = 0;
+	static int g = 50;
+	static int h = 50;  */
+	
+	static int a = 25;
+	static int b =25;
+	static int c = 47;
+	static int d = 47; 
+
+
 
 	public void settings()
 	{
@@ -12,11 +38,95 @@ public class Loops extends PApplet
 
 	public void setup() {
 		colorMode(HSB);
+	
+	
 		
 	}
 
 	
 	public void draw()
 	{	
+
+		/*Q1  int x = 0;
+		int y = 300;
+		int z = 200; */
+
+		/* Q2 int x = 0;
+		int y = 300;
+		int z = 200; */
+
+		/* Q3 int x = 0;
+		int y = 300;
+		int z = 200;  */
+
+		/* int x = 25;
+		int y = 300;
+		int z = 200; */ 
+
+		int x = 0;
+		int y = 300;
+		int z = 200;
+
+		
+
+		for(int i = 0; i < 10;i++)
+		{
+			/* Q1 fill(x,y,z);
+			rect(a,b,c, d);
+			noFill();
+			a += 50;
+			x += 19; */
+
+			/*Q2 fill(x,y,z);
+			rect(a,b,c, d);
+			noFill();
+			a += 50;
+			b += 50;
+			x += 25; */
+
+			/*  q3 fill(x,y,z);
+			rect(a,b,c,d);
+			rect(e,f,g,h); 
+			noFill();
+			a += 50;
+			b += 50;
+			e -= 50;
+			f += 50;
+			x += 25;
+
+			 */
+
+			/* Q4 fill(x,y,z);
+			ellipse(a, b, c, d);
+			noFill();
+			c += 5;
+			d += 5; 
+			x += 25; */
+
+			/* Q5 fill(x,y,z);
+			ellipse(a, b, c, d);
+			noFill();
+			a +=50;
+			x += 17; */
+
+			
+            /* Q6 for(int j = 0; j < 10; j++)
+            {
+                fill((255/100) * i * j, 255,255);
+                ellipse(((width/10) * i) + 25,((height/10)*j) + 25,width/10,height/10);
+            } */
+
+			
+            
+
+		
+			
+
+			
+			
+
+
+			
+		}
 	}
 }
